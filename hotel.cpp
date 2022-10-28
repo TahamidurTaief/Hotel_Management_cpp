78171a5cec0271b70567786277e776e0fe9a782b
Tahamidur Taief <tahamdiurtaief@gmail.com>
2022-10-28 15:33:08 +0600

    product quantity

--- hotel.cpp
+++ hotel.cpp
@@ -3,4 +3,38 @@ using namespace std;
 
 int main()
 {
+    int choice, quant;
+
+    // Quantitu of products
+    int Qrooms = 0, Qbread = 0, Qpasta = 0, Qburger = 0, Qnoodles = 0, Qshake = 0, Qjuice = 0, Qdrinks = 0;
+
+    // sell quantity of products
+    int Srooms = 0, Sbread = 0, Spasta = 0, Sburger = 0, Snoodles = 0, Sshake = 0, Sjuice = 0, Sdrinks = 0;
+
+    // total products
+    int Total_rooms = 0, Total_bread = 0, Total_pasta = 0, Total_burger = 0, Total_noodles = 0, Total_shake = 0, Total_joice = 0, Total_drinks = 0;
+
+    cout << "\n\t\tQuantity of the products we have.";
+    cout << "\n\t=================================================\n";
+
+    // product quantity insert
+    cout << "\n 1. Rooms : ";
+    cin >> Qrooms;
+    cout << "\n 2. Bread : ";
+    cin >> Qbread;
+    cout << "Pasta : ";
+    cin >> Qpasta;
+    cout << "Burger : ";
+    cin >> Qburger;
+    cout << "Noodles : ";
+    cin >> Qnoodles;
+    cout << "Shake : ";
+    cin >> Qshake;
+    cout << "juice : ";
+    cin >> Qjuice;
+    cout << "Drinks : ";
+    cin >> Qjuice;
+
+    // option printing
+    cout << "Select a option from the form : ";
 }
