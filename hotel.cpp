b35a14b062c6245e5738223a283000820baed0a4
Tahamidur Taief <tahamdiurtaief@gmail.com>
2022-10-28 20:51:31 +0600

    add all food function

--- hotel.cpp
+++ hotel.cpp
@@ -28,7 +28,7 @@ void bread(int Qbread, int Sbread, int Total_bread)
     if (Qbread - Sbread >= quant)
     {
         Sbread = Sbread + quant;
-        Total_bread = Total_bread + quant * 250;
+        Total_bread = Total_bread + quant * 80;
         cout << "\n\t" << quant << "Bread / breads is in cartlist.\n";
         cout << "\tYour bread price is : " << Total_bread << "\n\n";
     }
@@ -64,13 +64,85 @@ void burger(int Qburger, int Sburger, int Total_burger)
     if (Qburger - Sburger >= quant)
     {
         Sburger = Sburger + quant;
-        Total_burger = Total_burger + quant * 450;
+        Total_burger = Total_burger + quant * 300;
         cout << "\n\t" << quant << "burger in cartlist.\n";
         cout << "\tYour burger price is : " << Total_burger << "\n";
     }
 
     else
         cout << "\n\n\tOnly " << Qburger - Sburger << " are available in your stock.\n\n";
+}
+
+void noodles(int Qnoodles, int Snoodles, int Total_noodles)
+{
+    int quant;
+    cout << "\nHow many pasta you want : ";
+    cin >> quant;
+
+    if (Qnoodles - Snoodles >= quant)
+    {
+        Snoodles = Snoodles + quant;
+        Total_noodles = Total_noodles + quant * 150;
+        cout << "\n\t" << quant << "noodles in cartlist.\n";
+        cout << "\tYour noodles price is : " << Total_noodles << "\n";
+    }
+
+    else
+        cout << "\n\n\tOnly " << Qnoodles - Snoodles << " are available in your stock.\n\n";
+}
+
+void shake(int Qshake, int Sshake, int Total_shake)
+{
+    int quant;
+    cout << "\nHow many pasta you want : ";
+    cin >> quant;
+
+    if (Qshake - Sshake >= quant)
+    {
+        Sshake = Sshake + quant;
+        Total_shake = Total_shake + quant * 90;
+        cout << "\n\t" << quant << "shake in cartlist.\n";
+        cout << "\tYour shake price is : " << Total_shake << "\n";
+    }
+
+    else
+        cout << "\n\n\tOnly " << Qshake - Sshake << " are available in your stock.\n\n";
+}
+
+void juice(int Qjuice, int Sjuice, int Total_juice)
+{
+    int quant;
+    cout << "\nHow many pasta you want : ";
+    cin >> quant;
+
+    if (Qjuice - Sjuice >= quant)
+    {
+        Sjuice = Sjuice + quant;
+        Total_juice = Total_juice + quant * 100;
+        cout << "\n\t" << quant << "juice in cartlist.\n";
+        cout << "\tYour juice price is : " << Total_juice << "\n";
+    }
+
+    else
+        cout << "\n\n\tOnly " << Qjuice - Sjuice << " are available in your stock.\n\n";
+}
+
+void drinks(int Qdrinks, int Sdrinks, int Total_drinks)
+{
+    int quant;
+    cout << "\nHow many pasta you want : ";
+    cin >> quant;
+
+    if (Qdrinks - Sdrinks >= quant)
+    {
+        Sdrinks = Sdrinks + quant;
+        Total_drinks = Total_drinks + quant * 100;
+        cout << "\n\t" << quant << "drinks in cartlist.\n";
+        cout << "\tYour drinks price is : " << Total_drinks << "\n";
+    }
+
+    else
+        cout << "\n\n\tOnly " << Qdrinks - Sdrinks << " are available in your stock.\n\n";
 }
 
 int main()
@@ -84,7 +156,7 @@ int main()
     int Srooms = 0, Sbread = 0, Spasta = 0, Sburger = 0, Snoodles = 0, Sshake = 0, Sjuice = 0, Sdrinks = 0;
 
     // total products
-    int Total_rooms = 0, Total_bread = 0, Total_pasta = 0, Total_burger = 0, Total_noodles = 0, Total_shake = 0, Total_joice = 0, Total_drinks = 0;
+    int Total_rooms = 0, Total_bread = 0, Total_pasta = 0, Total_burger = 0, Total_noodles = 0, Total_shake = 0, Total_juice = 0, Total_drinks = 0;
 
     cout << "\n\t\tQuantity of the products we have.";
     cout << "\n\t=================================================\n";
@@ -117,6 +189,8 @@ int main()
     cout << "\t6. Shake\n";
     cout << "\t7. Juice\n";
     cout << "\t8. Drinks\n\n";
+    cout << "\t9. Total Informaion\n\n";
+    cout << "\t 10. exit\n\n";
 
     // work with user options
     cout << "Enter option number : ";
@@ -140,7 +214,24 @@ int main()
         burger(Qburger, Sburger, Total_burger);
         break;
 
-    default:
+    case 5:
+        noodles(Qnoodles, Snoodles, Total_noodles);
+        break;
+
+    case 6:
+        shake(Qshake, Sshake, Total_shake);
+        break;
+
+    case 7:
+        juice(Qjuice, Sjuice, Total_juice);
+        break;
+
+    case 8:
+        drinks(Qdrinks, Sdrinks, Total_drinks);
+        break;
+
+        default:
+        cout << "Invalid Options! Try again.";
         break;
     }
 }
